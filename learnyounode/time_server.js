714aa51b297c50819da4e16707ca6f75b272059c
Rohit Mishra <rohit@rohitmishra.me>
2014-03-14 18:43:16 +0530

    Add time_server

---
+++ learnyounode/time_server.js
@@ -0,0 +1,9 @@
+var net = require('net')
+var strftime = require('strftime')
+
+var server = net.createServer(function (socket) {
+  var date = new Date()
+  socket.end(strftime('%Y-%m-%d %H:%M\n'))
+})
+
+server.listen(process.argv[2])
